83a35eabb04be84aac127b82e7b0974209f3ad40
Ne02ptzero <louis@ne02ptzero.me>
2017-03-03 17:30:12 +0100

    Add(Tree)

---
+++ README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:980a3abca8672a780b16705ff9aea13038cc221621805774c52497ce5ed8f878
+size 1744
---
+++ usr/lib/gcc/x86_64-pc-linux-gnu/6.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../configure --prefix=/usr --enable-languages=c,c++ --disable-multilib --disable-bootstrap --with-system-zlib";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
